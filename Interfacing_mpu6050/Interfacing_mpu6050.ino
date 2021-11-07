d399e49afe0f846f3865a7a3e3f167f2744219e7
Jewook Ryu <ryu_jewook@berkeley.edu>
2021-11-06 18:14:59 -0700

    kalman filter added

--- Interfacing_mpu6050/Interfacing_mpu6050.ino
+++ Interfacing_mpu6050/Interfacing_mpu6050.ino
@@ -49,15 +49,16 @@ void loop() {
 //    Serial.print("gx: "); Serial.println(gx); Serial.print("\t");
 //    Serial.print("gy: "); Serial.println(gy); Serial.print("\t");
 //    Serial.print("gz: "); Serial.println(gz);
-  float angels[] = {0.0, 0.0, 0.0};
-  read_tilt_expo_mov(angels, gyro, 500, 0.25);
+  float angles[] = {0.0, 0.0, 0.0};
+//  /read_tilt_expo_mov(angles, gyro, 500, 0.25);
+  read_tilt_kalman(angles, gyro, 500);
   Serial.println("Tilt angles:");Serial.print("\t");
-  Serial.print("x-tilt: ");Serial.println(angels[0]);Serial.print("\t");
-  Serial.print("y-tilt: ");Serial.println(angels[1]);Serial.print("\t");
-  Serial.print("z-tilt: ");Serial.println(angels[2]);
-  angels[0] = 0;
-  angels[1] = 0;
-  angels[2] = 0;
+  Serial.print("x-tilt: ");Serial.println(angles[0]);Serial.print("\t");
+  Serial.print("y-tilt: ");Serial.println(angles[1]);Serial.print("\t");
+  Serial.print("z-tilt: ");Serial.println(angles[2]);
+  angles[0] = 0;
+  angles[1] = 0;
+  angles[2] = 0;
   delay(1000);
 }
 
@@ -70,7 +71,7 @@ void loop() {
   *       MPU6050 gyro      intialized object 
   *       int     samp_size number of iterution for doing avg sampling algorithm
   */
-void read_tilt_ave(float* angels, MPU6050 gyro, int samp_size) {
+void read_tilt_ave(float* angles, MPU6050 gyro, int samp_size) {
   // accelartion in "g"
   int16_t x, y, z;
   int16_t lax = 0;
@@ -83,9 +84,9 @@ void read_tilt_ave(float* angels, MPU6050 gyro, int samp_size) {
     laz += z;
   }
   lax /= samp_size;lay /= samp_size;laz /= samp_size;
-  angels[0] = atan(lax / (pow((pow(lay, 2) + pow(laz, 2)), 0.5))) * 180 / M_PI;
-  angels[1] = atan(lay / (pow((pow(lax, 2) + pow(laz, 2)), 0.5))) * 180 / M_PI;
-  angels[2] = atan((pow((pow(lay, 2) + pow(laz, 2)), 0.5)) / laz) * 180 / M_PI;
+  angles[0] = atan(lax / (pow((pow(lay, 2) + pow(laz, 2)), 0.5))) * 180 / M_PI;
+  angles[1] = atan(lay / (pow((pow(lax, 2) + pow(laz, 2)), 0.5))) * 180 / M_PI;
+  angles[2] = atan((pow((pow(lay, 2) + pow(laz, 2)), 0.5)) / laz) * 180 / M_PI;
 }
 
 
@@ -98,7 +99,7 @@ void read_tilt_ave(float* angels, MPU6050 gyro, int samp_size) {
   *       int     samp_size number of iterution for doing avg sampling algorithm
   *       float   alpha     weight of the most recent value
   */
-void read_tilt_expo_mov(float* angels, MPU6050 gyro, int samp_size, float alpha) {
+void read_tilt_expo_mov(float* angles, MPU6050 gyro, int samp_size, float alpha) {
   int16_t cur_x, cur_y, cur_z;
   int16_t x, y, z;
   gyro.getAcceleration(&x, &y, &z);
@@ -109,7 +110,44 @@ void read_tilt_expo_mov(float* angels, MPU6050 gyro, int samp_size, float alpha)
     cur_z = (alpha * z) + ((1-alpha) * cur_z);
     x = cur_x; y = cur_y; z = cur_z;
   }
-  angels[0] = atan(cur_x / (pow((pow(cur_y, 2) + pow(cur_z, 2)), 0.5))) * 180 / M_PI;
-  angels[1] = atan(cur_y / (pow((pow(cur_x, 2) + pow(cur_z, 2)), 0.5))) * 180 / M_PI;
-  angels[2] = atan((pow((pow(cur_y, 2) + pow(cur_z, 2)), 0.5)) / cur_z) * 180 / M_PI;
+  angles[0] = atan(cur_x / (pow((pow(cur_y, 2) + pow(cur_z, 2)), 0.5))) * 180 / M_PI;
+  angles[1] = atan(cur_y / (pow((pow(cur_x, 2) + pow(cur_z, 2)), 0.5))) * 180 / M_PI;
+  angles[2] = atan((pow((pow(cur_y, 2) + pow(cur_z, 2)), 0.5)) / cur_z) * 180 / M_PI;
  }
+
+/*
+  * Function to calculate tilt with Kalman filtered sample data
+  * 
+  * args :
+  *       float*  angles    pointer to array to store tilt_angles value
+  *       MPU6050 gyro      intialized object 
+  *       int     samp_size number of iterution for doing avg sampling algorithm
+  *       float   U         estimate
+  */
+void read_tilt_kalman(float* angles, MPU6050 gyro, int samp_size) {
+
+  static int16_t U[3] = {angles[0], angles[1], angles[2]};
+  static const double R = 40; /* noise covariance */
+  static const double H = 1.00; /* measurement map scalar */
+  static const double Q = 10; /* initial extimated covariance */
+  static double P = 0; /* initial error covariance */
+  static int16_t U_hat[3] = {0, 0, 0}; /* initial estimated state */
+  static double K = 0; /* Kalman gain */
+  
+  gyro.getAcceleration(&U[0], &U[1], &U[2]);
+  for (int i = 0; i < samp_size; ++i) {
+      gyro.getAcceleration(&U[0], &U[1], &U[2]);
+      
+      K = P * H / (H * P * H + R); /* update kalman */
+      U_hat[0] = U_hat[0] + K * (U[0] - H * U_hat[0]); /* update estimated */
+      U_hat[1] = U_hat[1] + K * (U[1] - H * U_hat[1]);
+      U_hat[2] = U_hat[2] + K * (U[2] - H * U_hat[2]);
+
+      /* update covariance */
+      P = (1 - K*H)*P + Q;
+
+  }
+  angles[0] = atan(U_hat[0] / (pow((pow(U_hat[1], 2) + pow(U_hat[2], 2)), 0.5))) * 180 / M_PI;
+  angles[1] = atan(U_hat[1] / (pow((pow(U_hat[0], 2) + pow(U_hat[2], 2)), 0.5))) * 180 / M_PI;
+  angles[2] = atan((pow((pow(U_hat[1], 2) + pow(U_hat[2], 2)), 0.5)) / U_hat[2]) * 180 / M_PI;
+ } 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # 106a-leapfrog
-EE 106A: Leap Frog POC
+EECS c106A (Introduction to Robotics), Fall 2021, Professor Koushil Sreenath, Professor Yi Ma
 
-In this project, we are trying to implement a self-balancing stick
+In this project, we are trying to implement a controller to balance an inverted pendulum (also known as a self-balancing stick).
