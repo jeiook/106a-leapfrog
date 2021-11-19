d923fb0046c4ec911d91cf0f2a0ced83908f93f0
Jewook Ryu <ryu_jewook@berkeley.edu>
2021-11-19 00:01:41 -0800

    determining min/max write values

--- interface_motor/interface_motor/interface_motor.ino
+++ interface_motor/interface_motor/interface_motor.ino
@@ -14,12 +14,26 @@ Servo ESC;      // Servo object to control ESC
 int potValue;   // Value of potentiometer from analog pin
 
 void setup() {
-  ESC.attach(9, 1000, 2000);    // (pin, min PWM, max PWM) in microseconds
+  ESC.attach(9);    // (pin, min PWM, max PWM) in microseconds
+  ESC.write(0);
+  delay(2000);
+  ESC.write(1200);
+  delay(2000);
+  Serial.begin(38400);
 }
+
+// set min pwm = 0, max pwm = 1000 -> write vals: min = 70, max = 180
 
 void loop() {
   // put your main code here, to run repeatedly:
-  potValue = analogRead(A0);
-  potValue = map(potValue, 0, 1023, 0, 180);    // Map the potentiometer value [0, 1023] -> [0,180]
-  ESC.write(potValue);
+//  potValue = analogRead(A0);
+//  potValue = map(potValue, 0, 1023, 0, 180); // Map the potentiometer value [0, 1023] -> [0,180]
+  for (int i = 0; i < 180; i += 2) {
+    ESC.write(i);
+    Serial.print(i);
+    Serial.print("\n");
+    delay(500);
+  }
+//  Serial.print(potValue);
+//  Serial.print("\n");
 }
